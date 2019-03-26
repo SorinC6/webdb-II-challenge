ddad005f1eecfd89b0938a2dd7a307a7f60040f8
Sorin Chis <kish.sorin@yahoo.com>
2019-03-26 14:13:13 +0200

    added knex , sqlite3 and knex connection to database file

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
 
-server.use(express.json());
+const db = knex({
+  client:'sqlite',
+  useNullAsDefault=true,
+  connection:{
+    filename:'./data/lambda.sqlite3'
+  }
+})
+
+const db = server.use(express.json());
 server.use(helmet());
 
 // endpoints here
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
