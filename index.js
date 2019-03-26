e115e1b2a84d03b57ec6ecddd438fab4ed0777f6
Sorin Chis <kish.sorin@yahoo.com>
2019-03-26 14:39:17 +0200

    added endpoint for getting a zo by id from the database

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ server.get('/api/zoos/', async (req, res) => {
 	}
 });
 
+server.get('/api/zoos/:id', async (req, res) => {
+	try {
+		const zoo = await db('zoos').where({ id: req.params.id }).first();
+		if (zoo) {
+			res.status(200).json(zoo);
+		} else {
+			res.status(404).json({ message: "the specified id does't exists" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'error trying to get the zoo by id' });
+	}
+});
+
+server.post('/api/zoos/', async (req, res) => {
+	try {
+		const [ result ] = await db('zoos').insert(req.body);
+
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(500).json({ error: 'There was a error trying to save the zoo to the database' });
+	}
+});
+
 const port = 3300;
 server.listen(port, function() {
 	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
